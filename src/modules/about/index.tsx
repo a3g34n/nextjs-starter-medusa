516eef9d86e43f1d4d7bec6b0cbf2075b20b22d2
a3g34n <egealsan99@gmail.com>
2026-03-20 21:40:40 +0300

    hakkimizda centered

--- src/modules/about/index.tsx
+++ src/modules/about/index.tsx
@@ -3,13 +3,13 @@ import Image from "next/image"
 export default function AboutPage() {
   return (
     <div className="bg-white pt-[60px] md:pt-20">
-      <div className="relative w-full">
+      <div className="relative w-full md:w-3/5 md:mx-auto">
         <Image
           src="/images/about/hakkimizde.webp"
           alt="Hakkımızda - LOUNJ Studio"
           width={0}
           height={0}
-          sizes="100vw"
+          sizes="(min-width: 768px) 60vw, 100vw"
           className="w-full h-auto"
           priority
         />
